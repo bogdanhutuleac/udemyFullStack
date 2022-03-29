8372a8bb440f8365c8adb5b7290db383b9f888fd
bogdanhutuleac <bogdanhutuleac@outlook.com>
2022-03-29 16:55:55 +0100

    Add classes

--- OOP/ConstructorFunction/script.js
+++ OOP/ConstructorFunction/script.js
@@ -48,30 +48,34 @@
 // constructor function
 /////////////////////////
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
-  console.log(this); //"this" will refer to the window
-  // this.rgb = function () {
-  //       const { r, g, b } = this;
-  //       // console.log(this);
-  //       return `rgb(${r},${g},${b})`;
-  //     };
-}
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+//   console.log(this); //"this" will refer to the window
+//   // this.rgb = function () {
+//   //       const { r, g, b } = this;
+//   //       // console.log(this);
+//   //       return `rgb(${r},${g},${b})`;
+//   //     };
+// }
 
-Color.prototype.rgb = function () {
-      const { r, g, b } = this;
-      // console.log(this);
-      return `rgb(${r},${g},${b})`;
-    };
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   // console.log(this);
+//   return `rgb(${r},${g},${b})`;
+// };
 
-Color.prototype.hex = function {
-  const { r, g, b } = this;
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-Color(255, 0, 0); //will return undefined
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+// console.log(Color(255, 0, 0));
+// Color(255, 0, 0); //will return undefined
 
+/////////////////////////
+// The new operator
+/////////////////////////
 // To make it work we need to introduce the new keyword - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
 
 // Creates a blank, plain JavaScript object.
@@ -79,7 +83,65 @@ Color(255, 0, 0); //will return undefined
 // Binds the newly created object instance as the this context (i.e. all references to this in the constructor function now refer to the object created in the first step).
 // Returns this if the function doesn't return an object.
 
-new Color(255,40,100) //new will work because will create a new object and "this" will refer to the new created object, not to the windows 
+// new Color(255, 40, 100); //new will work because will create a new object and "this" will refer to the new created object, not to the windows
 
-const color1 = new Color(40,60,20);
-const color2 = new Color(0,0,0);
+// const color1 = new Color(40, 60, 20);
+// const color2 = new Color(0, 0, 0);
+
+// console.log(color1.hex()); //#283c14
+// console.log(color2.hex()); //#000000
+
+// color1.hex === color2.hex; //true
+// // we return true now because we have defined .hex method under __proto__ , not just inside a function
+// console.log(color1.hex === color2.hex);
+
+// Color.prototype.rgba = function(a=1.0){
+//   const {r,g,b} = this;
+//   return `rgba(${r},${g},${b},${a})`;
+// }
+// console.log(color1.rgba(0.4))
+
+/////////////////////////
+// Classes
+/////////////////////////
+
+// For the name of the classes we use cap words
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+    // console.log('Inside Constructor')
+    // console.log(r,g,b)
+  }
+  // short version of adding a method
+  greet() {
+    return `Hello from ${this.name}`;
+  }
+  innerRGB() {
+    // destructure r,g,b from this
+    const { r, g, b } = this;
+    return `${r},${g},${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  rgba(a = 1) {
+    return `rgba(${this.innerRGB()},${a})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+}
+
+const red = new Color(255, 67, 89, "tomato");
+console.log(red);
+const white = new Color(255, 255, 255, "white");
+console.log(white.greet());
+console.log(white.hex());
+console.log(white.rgb());
+console.log(red.rgba(0.2));
+document.body.style.backgroundColor = red.rgba(0.5);
